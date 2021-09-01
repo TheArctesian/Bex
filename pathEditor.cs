e4b0737fa35b15537ade71c29e9820e27a19ccbb
TheArctesian <87273292+TheArctesian@users.noreply.github.com>
2021-09-01 10:41:35 +0800

    Kaitlyn is a bitch

---
+++ pathEditor.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PathCreator))]
+public class pathEditor : Editor {
+
+    PathCreator creator;
+    Path path;
+
+    void OnSceneGUI()
+    {
+        Input(); //call on input method to draw
+        Draw(); //draw to screen
+    }
+
+    void Input()
+    {
+        Event guiEvent = Event.current; 
+        //Get mouse position 
+        Vector2 mousePos = HandleUtility.GUIPointToWorldRay(guiEvent.mousePosition).origin; 
+
+        if (guiEvent.type == EventType.MouseDown && guiEvent.button == 0 && guiEvent.shift)
+        {
+            Undo.RecordObject(creator, "Add segment"); 
+            path.AddSegment(mousePos);
+            //add segment at mouse poriton 
+        }
+    }
+
+    void Draw()
+    {
+
+        for (int i = 0; i < path.NumSegments; i++)
+        {
+            Vector2[] points = path.GetPointsInSegment(i);
+            Handles.color = Color.black;
+            Handles.DrawLine(points[1], points[0]);
+            Handles.DrawLine(points[2], points[3]);
+            Handles.DrawBezier(points[0], points[3], points[1], points[2], Color.white, null, 2);
+        }
+
+        Handles.color = Color.red;
+        for (int i = 0; i < path.NumPoints; i++)
+        {
+            Vector2 newPos = Handles.FreeMoveHandle(path[i], Quaternion.identity, .1f, Vector2.zero, Handles.CylinderHandleCap); 
+            // get point lock rotation and move with mouse movement
+            if (path[i] != newPos)
+            {
+                Undo.RecordObject(creator, "Move point");
+                path.MovePoint(i, newPos);
+                debug.Log("Postion=" + newPos);
+            }
+        }
+    }
+
+    void OnEnable()
+    {
+        creator = (PathCreator)target;
+        if (creator.path == null)
+        {
+            creator.CreatePath();
+        }
+        path = creator.path;
+    }
+}
